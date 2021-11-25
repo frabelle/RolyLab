a40e8ed695e6c0a1ba257979698c427f431a52e4
frabelle <renchifrabelle@gmail.com>
2021-11-24 22:03:20 -0600

    agregando lo de services y actions

--- src/components/tools/Styles.js
+++ src/components/tools/Styles.js
@@ -1,19 +1,19 @@
 const style = {
-    paper:{
-        marginTop : 8,
+    paper: {
+        marginTop: 8,
         display: "flex",
         flexDirection: "column",
         alignItem: "center"
     },
     form: {
-        width : "100%",
+        width: "100%",
         marginTop: 20
     },
     submit: {
         marginTop: 15
     },
-    avatar:{
-        margin:5,
+    avatar: {
+        margin: 5,
         backgroundColor: "#1976d2",
         width: 60,
         height: 60,
