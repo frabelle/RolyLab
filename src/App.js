7cf36154893110392dc03d3431539a245ceffd57
frabelle <renchifrabelle@gmail.com>
2021-11-24 23:35:16 -0600

    actualizando el form de agregar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import theme from './components/tools/Theme';
 import RegistrarExamen from './components/exams/Registrar';
 import ListarExamen from './components/exams/Lista'
+import Form from './pages/FormInsert'
 
 function App() {
   return (
     <MuithemeProvider theme={theme}>
-      <h1 style={{textAlign:"center"}}>Bienvenidos a ROLY</h1>
-      <RegistrarExamen />
-      <ListarExamen/>
+      <h1 style={{textAlign:"center"}}></h1>
+      
+      <RegistrarExamen/>
     </MuithemeProvider>
   );
 }
--- src/components/exams/Registrar.js
+++ src/components/exams/Registrar.js
@@ -1,129 +1,157 @@
 import { Button, Container, Grid, Select, TextField, Typography, InputLabel, FormControl, MenuItem} from '@mui/material';
 import React from 'react';
 import styles from '../tools/Styles';
+import {
+    Box,
+    Card,
+    CardContent,
+    CardHeader,
+    Divider
+  } from '@mui/material';
 
 const RegistrarExamen = () =>{
     return(
         <Container component="main" maxWidth="lg" justify = "center">
             <div style={styles.paper}>
 
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={6}>
-                        <Typography component="h1" variant="h5" style={{textAlign:"left"}}>
-                            Registrar un examen
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Typography component="h6" style={{textAlign:"right"}}>
-                            Regresar a la página anterior
-                        </Typography>
-                    </Grid>
-                </Grid>
-                
-                <form style={styles.form}>
+                <Card>
 
-                    <Grid container spacing={3}>
+                    <CardHeader
+                    subheader="Añadiendo un nuevo registro de examen"
+                    title="Exámenes"
+                    />
 
-                        <Grid item xs={12} md={6}>
-                            <TextField name="nombre" variant="outlined" fullWidth label="Nombre del examen" />
-                        </Grid>
+                    <Divider />
 
-                        <Grid item xs={12} md={6}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Área de servicio</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    label="area"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
+                    <CardContent>
+                        <form style={styles.form}>
+                            <Grid container spacing={3}>
 
-                        <Grid item xs={12} md={3}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Categoría</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    label="area"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
+                                <Grid item xs={12} md={6}>
+                                    <TextField name="nombre" variant="outlined" fullWidth label="Nombre del examen" />
+                                </Grid>
 
-                        <Grid item xs={12} md={3}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Tipo de muestra</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    label="area"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
+                                <Grid item xs={12} md={6}>
+                                    <TextField
+                                        fullWidth
+                                        label="Área de servicio"
+                                        name="state"
+                                        required
+                                        select
+                                        SelectProps={{ native: true }}
+                                        variant="outlined"
+                                        >
 
-                        <Grid item xs={12} md={3}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Unidad de medida</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    label="area"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                        
+                                    </TextField>
+                                </Grid>
 
-                        <Grid item xs={12} md={3}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Tipo de resultado</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    label="area"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
+                                <Grid item xs={12} md={3}>
+                                    <TextField
+                                        fullWidth
+                                        label="Categoría"
+                                        name="state"
+                                        required
+                                        select
+                                        SelectProps={{ native: true }}
+                                        variant="outlined"
+                                        >
 
-                        <Grid item xs={12} md={12}>
-                            <TextField name="descripcion" variant="outlined" fullWidth label="Descripción" />
-                        </Grid>
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                        
+                                    </TextField>
+                                </Grid>
 
-                        <Grid item xs={12} md={6}>
-                            <Button type="submit" fullWidth variant="contained" color="primary" size="large" style={styles.submit}>
+                                <Grid item xs={12} md={3}>
+                                    <TextField
+                                        fullWidth
+                                        label="Tipo de muestra"
+                                        name="state"
+                                        required
+                                        select
+                                        SelectProps={{ native: true }}
+                                        variant="outlined"
+                                        >
+
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                        
+                                    </TextField>
+                                </Grid>
+
+                                <Grid item xs={12} md={3}>
+                                    <TextField
+                                        fullWidth
+                                        label="Unidad de medida"
+                                        name="state"
+                                        required
+                                        select
+                                        SelectProps={{ native: true }}
+                                        variant="outlined"
+                                        >
+
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                        
+                                    </TextField>
+                                </Grid>
+
+                                <Grid item xs={12} md={3}>
+                                    <TextField
+                                        fullWidth
+                                        label="Tipo de resultado"
+                                        name="state"
+                                        required
+                                        select
+                                        SelectProps={{ native: true }}
+                                        variant="outlined"
+                                        >
+
+                                        <MenuItem value={10}>Ten</MenuItem>
+                                        <MenuItem value={20}>Twenty</MenuItem>
+                                        <MenuItem value={30}>Thirty</MenuItem>
+                                        
+                                    </TextField>
+                                </Grid>
+
+                                <Grid item xs={12} md={12}>
+                                    <TextField name="descripcion" variant="outlined" fullWidth label="Descripción" />
+                                </Grid>
+
+                            </Grid>
+                        </form>
+                    </CardContent>
+
+                    <Divider />
+
+                    <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        p: 2
+                    }}
+                    >
+                        <Grid item xs={12} md={2}>
+                            <Button type="submit" fullWidth variant="contained" color="primary">
                                 Guardar cambios
                             </Button>
                         </Grid>
 
-                        <Grid item xs={12} md={6}>
-                            <Button type="submit" fullWidth variant="contained" color="warning" size="large" style={styles.submit}>
-                                Cancelar
-                            </Button>
-                        </Grid>
-
-                    </Grid>
-
-                </form>
+                    </Box>
+                </Card>
+           
             </div>
         </Container>
+
+      
+
     )
 }
 
